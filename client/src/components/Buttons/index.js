1aa4fc39d8057fb2b3321759e4732f60b8334807
richardBoyer917 <yuriAkechi4802@gmail.com>
2024-11-08 15:12:21 -0500

    delete Button of MUI in TabButton

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -1,158 +1,207 @@
-import { Link as ScrollLink } from "react-scroll"
-import { Button } from '@mui/material'
-import { darkArrowup, darkTelegramRound, lightArrow } from '../../assets'
-import "../../styles/components/button.css"
+import { Link as ScrollLink } from "react-scroll";
+import { darkArrowup, darkTelegramRound, lightArrow } from "../../assets";
+import "../../styles/components/button.css";
 
 const ScrollSpyButton = ({ to, content }) => (
-  <ScrollLink to={to} offset={-200} smooth={true} spy={true} >{content}</ScrollLink>
-)
+  <ScrollLink to={to} offset={-200} smooth={true} spy={true}>
+    {content}
+  </ScrollLink>
+);
 
 const DefaultButton = ({ type, onClick, title }) => (
-  <button type={type} onClick={onClick} className="button defaultButton" >{title}</button>
-)
+  <button type={type} onClick={onClick} className="button defaultButton">
+    {title}
+  </button>
+);
 
 const BlackButton = ({ title, onClick }) => (
-  <button className="button defaultButton blackButton" onClick={onClick} >{title}</button>
-)
+  <button className="button defaultButton blackButton" onClick={onClick}>
+    {title}
+  </button>
+);
 
 const TabButton1 = ({ icon, title, onClick }) => (
-  <button className='button tabButton' onClick={onClick} style={{ background: `var(--secondaryWhiteColor)` }}><img src={icon} alt='darkAdd' />{title}</button>
-)
+  <button
+    className="button tabButton"
+    onClick={onClick}
+    style={{ background: `var(--secondaryWhiteColor)` }}
+  >
+    <img src={icon} alt="darkAdd" />
+    {title}
+  </button>
+);
 
 const TabButton = ({ icon, title, onChange }) => (
-  <Button
-    component="label"
-    sx={{
-      display: 'flex',
-      alignItems: 'center',
-      border: 'none',
-      gap: '10px',
-      textAlign: 'center',
-      transition: '0.3s',
-      cursor: 'pointer',
-      height: '40px',
-      borderRadius: '5px',
-      padding: '10px 20px',
-      backgroundColor: 'white',
-      fontFamily: 'Mulish',
-      color: 'black',
-      fontSize: 'clamp(14px, 1vw, 16px)',
-      fontWeight: '700',
-      lineHeight: '20.08px',
-      letterSpacing: '-0.03em',
-      textTransform: 'none',
-      '&:hover': {
-        backgroundColor: 'var(--secondaryWhiteHover)', // Define hover state
-      },
-    }}
-  >
+  <label className="tabButton">
     <input type="file" name="video" hidden onChange={onChange} />
     <img src={icon} alt="darkAdd" />
     {title}
-  </Button>
-)
+  </label>
+);
 
 const SmallTabButton = ({ title, onClick }) => (
-  <button onClick={onClick} className='button tabButton smallTabButton'>{title}</button>
-)
+  <button onClick={onClick} className="button tabButton smallTabButton">
+    {title}
+  </button>
+);
 
 const CircleButton = ({ icon, scale }) => (
-  <button className="button circleButton" style={{ scale: scale }}><img src={icon} alt='darkTelegram' /></button>
-)
+  <button className="button circleButton" style={{ scale: scale }}>
+    <img src={icon} alt="darkTelegram" />
+  </button>
+);
 
 const OutLinedButton = ({ onClick, title }) => (
-  <button onClick={onClick} className="button defaultButton outLinedButton" >{title}</button>
-)
+  <button onClick={onClick} className="button defaultButton outLinedButton">
+    {title}
+  </button>
+);
 
 const BigTransButton = ({ title, long, onClick }) => (
-  <button className={`button transButton bigTransButton ${long ? 'bigTransButtonPadding' : ''}`} onClick={onClick} >{title}</button>
-)
+  <button
+    className={`button transButton bigTransButton ${
+      long ? "bigTransButtonPadding" : ""
+    }`}
+    onClick={onClick}
+  >
+    {title}
+  </button>
+);
 
 const SmallTransButton = ({ title }) => (
-  <button className='button transButton smallTransButton'>{title}</button>
-)
+  <button className="button transButton smallTransButton">{title}</button>
+);
 
 const HeroTopWhiteButton = ({ title }) => (
-  <button className='button heroTopWhitebutton'>{title}</button>
-)
+  <button className="button heroTopWhitebutton">{title}</button>
+);
 
 const RectButton = ({ onClick, title }) => (
-  <button onClick={onClick} className="button rectButton" >{title}</button>
-)
+  <button onClick={onClick} className="button rectButton">
+    {title}
+  </button>
+);
 
 const ArrowDefaultButton = ({ title, onClick }) => (
-  <button onClick={onClick} className="button defaultButton " style={{ height: '39px', paddingRight: '24px', paddingLeft: '24px' }}>{title}<img src={darkArrowup} alt='arrowButton' /></button>
-)
+  <button
+    onClick={onClick}
+    className="button defaultButton "
+    style={{ height: "39px", paddingRight: "24px", paddingLeft: "24px" }}
+  >
+    {title}
+    <img src={darkArrowup} alt="arrowButton" />
+  </button>
+);
 
 const ArrowBlackButton = ({ title, onClick }) => (
-  <button onClick={onClick} className="button defaultButton blackButton" style={{ height: '39px', paddingRight: '24px', paddingLeft: '24px' }}>{title}<img src={lightArrow} alt='arrowButton' /></button>
-)
+  <button
+    onClick={onClick}
+    className="button defaultButton blackButton"
+    style={{ height: "39px", paddingRight: "24px", paddingLeft: "24px" }}
+  >
+    {title}
+    <img src={lightArrow} alt="arrowButton" />
+  </button>
+);
 
 const BigArrowBlackButton = ({ title }) => (
-  <button className="button defaultButton blackButton" style={{ height: '48px' }}>{title}<img src={darkTelegramRound} alt='arrowButton' /></button>
-)
+  <button
+    className="button defaultButton blackButton"
+    style={{ height: "48px" }}
+  >
+    {title}
+    <img src={darkTelegramRound} alt="arrowButton" />
+  </button>
+);
 
 const HeroTopButton = ({ title }) => (
-  <button className='button heroTopbutton'>{title}</button>
-)
+  <button className="button heroTopbutton">{title}</button>
+);
 
 const SmallHeroLinkButton = ({ title }) => (
-  <button className='button smallHeroButton heroTopbutton'>{title}</button>
-)
+  <button className="button smallHeroButton heroTopbutton">{title}</button>
+);
 
-const LinkButton = ({ title }) => (
-  <span className='linkButton'>{title}</span>
-)
+const LinkButton = ({ title }) => <span className="linkButton">{title}</span>;
 
 const DownloadButton = ({ icon, title, onClick }) => (
-  <button className='button blackButton downloadBtn' onClick={onClick}>
-    <img src={icon} alt='icon' /> {title}
+  <button className="button blackButton downloadBtn" onClick={onClick}>
+    <img src={icon} alt="icon" /> {title}
   </button>
-)
+);
 
 const DownloadButton1 = ({ icon, title, onClick }) => (
-  <button className='button blackButton downloadBtn x16Font_5' style={{ height: '34px', background: '#EBEBEB', color: `var(--primaryBgColor)`, padding: '11.5px clamp(10px, 2vw, 24px)' }} onClick={onClick}>
-    <img src={icon} alt='icon' /> {title}
+  <button
+    className="button blackButton downloadBtn x16Font_5"
+    style={{
+      height: "34px",
+      background: "#EBEBEB",
+      color: `var(--primaryBgColor)`,
+      padding: "11.5px clamp(10px, 2vw, 24px)",
+    }}
+    onClick={onClick}
+  >
+    <img src={icon} alt="icon" /> {title}
   </button>
-)
+);
 
 const HeroDarkButton = ({ title }) => (
-  <button className='button defaultButton blackButton heroDarkButton'>{title}</button>
-)
+  <button className="button defaultButton blackButton heroDarkButton">
+    {title}
+  </button>
+);
 
 const DarkIconButton = ({ icon, title }) => (
-  <button className='button blackButton downloadBtn' style={{ fontSize: '12px', padding: '8px 10px' }}><img src={icon} alt='icon' />{title}</button>
-)
+  <button
+    className="button blackButton downloadBtn"
+    style={{ fontSize: "12px", padding: "8px 10px" }}
+  >
+    <img src={icon} alt="icon" />
+    {title}
+  </button>
+);
 
 const QuestionButton = ({ title }) => (
   <button className="button questionButton">{title}</button>
-)
+);
 
 const EventTagButton = ({ title }) => (
-  <button className="button transButton eventTags" >{title}</button>
-)
+  <button className="button transButton eventTags">{title}</button>
+);
 
 const BlackButtonBorderWhite = ({ onClick, title }) => (
-  <button onClick={onClick} className="button defaultButton blackButton" style={{ border: '1px solid white' }} >{title}</button>
-)
+  <button
+    onClick={onClick}
+    className="button defaultButton blackButton"
+    style={{ border: "1px solid white" }}
+  >
+    {title}
+  </button>
+);
 
 const CaseButton = ({ title }) => (
-  <button className='button caseButton'>{title}</button>
-)
+  <button className="button caseButton">{title}</button>
+);
 
 const Banquet = ({ title }) => (
-  <button className='button banquetBtn'>{title}</button>
-)
+  <button className="button banquetBtn">{title}</button>
+);
 
 export {
   ScrollSpyButton,
-  DefaultButton, BlackButton,
-  TabButton1, TabButton, SmallTabButton,
+  DefaultButton,
+  BlackButton,
+  TabButton1,
+  TabButton,
+  SmallTabButton,
   CircleButton,
   OutLinedButton,
-  SmallTransButton, BigTransButton,
+  SmallTransButton,
+  BigTransButton,
   RectButton,
-  ArrowDefaultButton, ArrowBlackButton, BigArrowBlackButton,
+  ArrowDefaultButton,
+  ArrowBlackButton,
+  BigArrowBlackButton,
   HeroTopButton,
   SmallHeroLinkButton,
   LinkButton,
@@ -166,4 +215,4 @@ export {
   BlackButtonBorderWhite,
   CaseButton,
   Banquet,
-}
+};
--- client/src/components/Cards/CoveringPreview.js
+++ client/src/components/Cards/CoveringPreview.js
@@ -3,7 +3,7 @@ import Modal from "@mui/material/Modal";
 import CloseIcon from "@mui/icons-material/Close";
 
 const CoveringPreview = (props) => {
-  const { open, setOpen, avatar, name } = props
+  const { open, setOpen, avatar, name } = props;
 
   const handleClose = () => setOpen(false);
   return (
@@ -14,13 +14,16 @@ const CoveringPreview = (props) => {
       aria-describedby="modal-modal-description"
       disableAutoFocus
       sx={{
-        backgroundColor: 'rgba(23, 23, 23, 0.42)', // Semi-transparent background color
-        backdropFilter: 'blur(8px)',  // Blur effect
+        backgroundColor: "rgba(23, 23, 23, 0.42)", // Semi-transparent background color
+        backdropFilter: "blur(8px)", // Blur effect
       }}
     >
       <div className="videoPreviewContainer">
         <div className="spaceBetween">
-          <p className="cardBigTitle" style={{ color: 'var(--secondaryWhiteColor)' }}>
+          <p
+            className="cardBigTitle"
+            style={{ color: "var(--secondaryWhiteColor)" }}
+          >
             {name}
           </p>
           <IconButton onClick={handleClose}>
@@ -28,13 +31,21 @@ const CoveringPreview = (props) => {
           </IconButton>
         </div>
         <div>
-          <video style={{ width: '100%', height: '65%', marginTop: '20px', borderRadius: '5px' }} controls>
+          <video
+            style={{
+              width: "100%",
+              height: "65%",
+              marginTop: "20px",
+              borderRadius: "5px",
+            }}
+            controls
+          >
             <source src={avatar} type="video/mp4" />
           </video>
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default CoveringPreview
+export default CoveringPreview;
